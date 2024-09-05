933c2c70b53de3ff9e4a1f5e3bb2a1355f471635
SOE ZAW AUNG <133962444+Scott2106@users.noreply.github.com>
2024-09-05 10:27:52 +0800

    updated cors policy ( allow custom subdomains to make api call)

--- GLF-BackEnd-/app.js
+++ GLF-BackEnd-/app.js
@@ -32,6 +32,8 @@ app.use((req, res, next) => {
   const allowedOrigins = [
     process.env.LOCAL_BACKEND_URL,
     process.env.BACKEND_URL,
+    process.env.FRONTEND_ENDUSER_URL,
+    process.env.FRONTEND_ADMIN_URL,
     // Add more domains as needed
   ];
 

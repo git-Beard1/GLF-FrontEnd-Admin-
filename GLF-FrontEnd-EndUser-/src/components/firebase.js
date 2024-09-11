d178068a68ac4f129eb2ecc736d9e130923c6cf4
SOE ZAW AUNG <133962444+Scott2106@users.noreply.github.com>
2024-09-11 15:32:51 +0800

    Go back to original firebase config

--- GLF-FrontEnd-EndUser-/src/components/firebase.js
+++ GLF-FrontEnd-EndUser-/src/components/firebase.js
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,7 +11,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
